2a0a8a1a2ec5d8b27a581e4b1e7030811f1a3c8d
emanuele23dev <emanuele.meggiarin@libero.it>
2024-11-28 11:25:08 +0100

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="container">
+      <h1 className="title">React Blog Pages</h1>
+      <img className="logo" src="../src/assets/react.svg" alt="" />
+    </div>
+  );
+}
